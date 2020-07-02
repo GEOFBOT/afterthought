02ca4337bc594f267523d10b9fc8ddd5a2efaf36
Geoffrey Mon <geofbot@gmail.com>
2020-07-02 14:20:29 -0400

    programs with add

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub enum Token {
     Int(i32),
     LiteralName(String),
@@ -5,6 +6,7 @@ pub enum Token {
     Name(String),
 }
 
+#[derive(Debug)]
 pub enum Program {
     Concat(Token, Box<Program>),
     End,
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,46 @@ extern crate lalrpop_util;
 
 pub mod ast;
 
+use ast::{Program, Token};
 lalrpop_mod!(pub postscript);
 
 fn main() {}
+
+fn execute(p: Program) -> Option<i32> {
+    let mut cur = &p;
+    let mut operand_stack = Vec::<i32>::new();
+    loop {
+        match cur {
+            Program::End => {
+                break;
+            }
+            Program::Concat(t, b) => {
+                match t {
+                    Token::Int(i) => {
+                        operand_stack.push(*i);
+                    }
+                    Token::Name(n) => match n.as_str() {
+                        "add" => {
+                            let a = operand_stack.pop().unwrap();
+                            let b = operand_stack.pop().unwrap();
+                            operand_stack.push(a + b);
+                        }
+                        _ => {
+                            unimplemented!();
+                        }
+                    },
+                    _ => {
+                        unimplemented!();
+                    }
+                };
+
+                cur = b;
+            }
+        }
+    }
+
+    return operand_stack.pop();
+}
 
 #[test]
 fn test_token_terminals() {
@@ -32,5 +69,16 @@ fn test_token_terminals() {
 
 #[test]
 fn test_many_tokens() {
-    assert!(postscript::ProgramParser::new().parse("10 13 add").is_ok())
+    assert!(postscript::ProgramParser::new().parse("10 13 add").is_ok());
+}
+
+#[test]
+fn test_basic() {
+    let p = postscript::ProgramParser::new().parse("10 11 add").unwrap();
+    assert!(execute(p) == Some(10 + 11));
+
+    let p = postscript::ProgramParser::new()
+        .parse("10 11 12 13 add add add")
+        .unwrap();
+    assert!(execute(p) == Some(10 + 11 + 12 + 13));
 }
